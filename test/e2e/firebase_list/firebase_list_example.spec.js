b7f0237f1c63157805015773b2b32b0c27c0805b
Jeff Cross <middlefloor@gmail.com>
2016-01-25 06:49:04 -0800

    test: add angular and system to e2e test

--- test/e2e/firebase_list/firebase_list_example.spec.js
+++ test/e2e/firebase_list/firebase_list_example.spec.js
@@ -1,11 +1,20 @@
+/* global protractor */
 /* global $ */
 /* global element */
 /* global browser */
+function waitForElement(selector) {
+  var EC = (protractor).ExpectedConditions;
+  // Waits for the element with id 'abc' to be present on the dom.
+  browser.wait(EC.presenceOf($(selector)), 20000);
+}
+
 describe('FirebaseList', () => {
   it('should render', () => {
-    console.log('render test');
     browser.get('firebase_list/index.html');
+    waitForElement('app');
     var app = $('app');
-    expect(app.getText()).toBe('Hello');
+    expect(app.getText()).toBe(`Hello
+why?
+how?`);
   });
 });
