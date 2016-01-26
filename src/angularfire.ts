534a6f6a5f21456a942750d1dabd0665888877b7
Jeff Cross <middlefloor@gmail.com>
2016-01-26 13:10:40 -0800

    chore(npm): publish within dist folder with default export

--- src/angularfire.ts
+++ src/angularfire.ts
@@ -14,3 +14,8 @@ export const FIREBASE_PROVIDERS:any[] = [
 
 export {FirebaseList, FirebaseListConfig} from './providers/firebase_list';
 export {FirebaseObservable} from './utils/firebase_observable';
+
+// Helps Angular-CLI automatically add providers
+export default {
+  providers: FIREBASE_PROVIDERS
+}
