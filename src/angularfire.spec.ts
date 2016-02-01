dddd0b3646e5afe7f9e068f6158e3c62c6c3552c
David East <david@firebase.com>
2016-02-01 11:23:31 -0800

    chore(providers): shortcut for configuring DEFAULT_FIREBASE provider

--- src/angularfire.spec.ts
+++ src/angularfire.spec.ts
@@ -1,17 +1,26 @@
-import {describe,it,beforeEach} from 'angular2/testing';
-import {Injector, provide} from 'angular2/core';
-import {FIREBASE_PROVIDERS, FirebaseUrl, FirebaseRef} from './angularfire';
+import {describe,it,beforeEach, expect} from 'angular2/testing';
+import {Injector, provide, Provider} from 'angular2/core';
+import {FIREBASE_PROVIDERS, FirebaseUrl, FirebaseRef, defaultFirebase} from './angularfire';
+
+const testUrl = 'https://ng2-forum-demo.firebaseio.com';
 
 describe('angularfire', () => {
   describe('FIREBASE_REF', () => {
     it('should provide a FirebaseRef for the FIREBASE_REF binding', () => {
       var injector = Injector.resolveAndCreate([
         provide(FirebaseUrl, {
-          useValue: 'https://ng2-forum-demo.firebaseio.com'
+          useValue: testUrl
         }),
         FIREBASE_PROVIDERS
       ]);
       expect(typeof injector.get(FirebaseRef).on).toBe('function');
     })
+  });
+  
+  describe('defaultFirebase', () => {
+    it('should create a provider', () => {
+      const provider = defaultFirebase(testUrl);
+      expect(provider).toBeAnInstanceOf(Provider);
+    });
   });
 });
--- src/angularfire.ts
+++ src/angularfire.ts
@@ -10,6 +10,12 @@ export const FIREBASE_PROVIDERS:any[] = [
     deps: [FirebaseUrl]})
 ];
 
+export const defaultFirebase = (url: string) => {
+  return provide(FirebaseUrl, {
+    useValue: url
+  });
+};
+
 export {FirebaseList, FirebaseListConfig} from './providers/firebase_list';
 export {FirebaseObservable} from './utils/firebase_observable';
 
