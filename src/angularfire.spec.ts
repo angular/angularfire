118e99a0e1879cfe2f863a7c709450350cb3ede7
Jeff Cross <middlefloor@gmail.com>
2016-02-02 17:42:44 -0800

    feat(AngularFire): add AngularFire service with list method

--- src/angularfire.spec.ts
+++ src/angularfire.spec.ts
@@ -1,10 +1,35 @@
-import {describe,it,beforeEach, expect, inject} from 'angular2/testing';
+import {
+  describe,
+  it,
+  beforeEach,
+  beforeEachProviders,
+  expect,
+  inject
+} from 'angular2/testing';
 import {Injector, provide, Provider} from 'angular2/core';
-import {FIREBASE_PROVIDERS, FirebaseUrl, FirebaseRef, defaultFirebase} from './angularfire';
+import {AngularFire, FIREBASE_PROVIDERS, FirebaseUrl, FirebaseRef, defaultFirebase} from './angularfire';
 
-const testUrl = 'https://ng2-forum-demo.firebaseio.com/';
+const testUrl = 'http://test.firebaseio.com:5000/';
 
 describe('angularfire', () => {
+  it('should be injectable via FIREBASE_PROVIDERS', () => {
+    var injector = Injector.resolveAndCreate([FIREBASE_PROVIDERS, defaultFirebase(testUrl)]);
+    expect(injector.get(AngularFire)).toBeAnInstanceOf(AngularFire);
+  });
+
+  describe('.list()', () => {
+    beforeEachProviders(() => [FIREBASE_PROVIDERS, defaultFirebase(testUrl)]);
+
+    it('should return an observable of the path', inject([AngularFire], (af:AngularFire) => {
+      var nextSpy = jasmine.createSpy('next');
+      var questions = af.list('list-of-questions');
+      questions.subscribe(nextSpy);
+      questions.add(['hello']);
+      expect(nextSpy.calls.first().args[0][0].val()).toEqual(['hello']);
+    }));
+  });
+
+
   describe('FIREBASE_REF', () => {
     it('should provide a FirebaseRef for the FIREBASE_REF binding', () => {
       var injector = Injector.resolveAndCreate([
@@ -16,17 +41,18 @@ describe('angularfire', () => {
       expect(typeof injector.get(FirebaseRef).on).toBe('function');
     })
   });
-  
+
   describe('defaultFirebase', () => {
     it('should create a provider', () => {
       const provider = defaultFirebase(testUrl);
       expect(provider).toBeAnInstanceOf(Provider);
     });
-    
+
+
     it('should inject a FIR reference', () => {
       const injector = Injector.resolveAndCreate([defaultFirebase(testUrl), FIREBASE_PROVIDERS]);
-      expect(injector.get(FirebaseRef).toString()).toBe(testUrl);  
+      expect(injector.get(FirebaseRef).toString()).toBe(testUrl);
     });
   });
-  
+
 });
--- src/angularfire.ts
+++ src/angularfire.ts
@@ -1,13 +1,26 @@
-import {OpaqueToken, provide, Provider} from 'angular2/core';
+import {Inject, Injectable, OpaqueToken, provide, Provider} from 'angular2/core';
 import {FirebaseAuth} from './providers/auth';
 import * as Firebase from 'firebase';
+import {FirebaseObservable} from './utils/firebase_observable';
+import {FirebaseListFactory} from './utils/firebase_list_factory';
 import {FirebaseUrl, FirebaseRef} from './tokens';
+
+@Injectable()
+export class AngularFire {
+  constructor(@Inject(FirebaseUrl) private fbUrl:string) {
+  }
+  list (url:string):FirebaseObservable<any[]> {
+    // TODO: check if relative or absolute
+    return FirebaseListFactory(this.fbUrl+url);
+  }
+}
 
 export const FIREBASE_PROVIDERS:any[] = [
   provide(FirebaseRef, {
     useFactory: (url:string) => new Firebase(url),
     deps: [FirebaseUrl]}),
-  FirebaseAuth
+  FirebaseAuth,
+  AngularFire
 ];
 
 export const defaultFirebase = (url: string): Provider => {
