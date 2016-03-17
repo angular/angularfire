0c9100787569317db3da2d9196b0ec146654befd
Jeff Cross <middlefloor@gmail.com>
2016-03-16 17:44:11 -0700

    fix(e2e): fix firebase list e2e test

--- test/e2e/firebase_list/firebase_list_example.spec.js
+++ test/e2e/firebase_list/firebase_list_example.spec.js
@@ -13,6 +13,7 @@ describe('FirebaseList', () => {
     browser.get('firebase_list/index.html');
     waitForElement('app');
     var app = $('app');
+    browser.sleep(500);
     expect(app.getText()).toBe(`Hello
 why?
 how?`);
