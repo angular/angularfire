3be0dedd0ede010444867b5e0a36e51e26083b7f
David East <david@firebase.com>
2016-03-24 06:15:03 -0700

    feat(auth): Rename provider folder to auth

--- src/angularfire2_worker_app.ts
+++ src/angularfire2_worker_app.ts
@@ -1,7 +1,7 @@
 import {provide} from 'angular2/core';
 import {COMMON_PROVIDERS} from './angularfire2';
-import {AuthBackend} from './providers/auth_backend';
-import {WebWorkerFirebaseAuth} from './providers/web_workers/worker/auth';
+import {AuthBackend} from './auth/auth_backend';
+import {WebWorkerFirebaseAuth} from './auth/web_workers/worker/auth';
 
 export const WORKER_APP_FIREBASE_PROVIDERS: any[] = [
   COMMON_PROVIDERS,
--- src/angularfire2_worker_render.ts
+++ src/angularfire2_worker_render.ts
@@ -1,9 +1,9 @@
 import {provide} from 'angular2/core';
 import {COMMON_PROVIDERS} from './angularfire2';
-import {FirebaseSdkAuthBackend} from './providers/firebase_sdk_auth_backend';
-import {WebWorkerFirebaseAuth} from './providers/web_workers/worker/auth';
+import {FirebaseSdkAuthBackend} from './auth/firebase_sdk_auth_backend';
+import {WebWorkerFirebaseAuth} from './auth/web_workers/worker/auth';
 import {FirebaseRef} from './tokens';
-import {MessageBasedFirebaseAuth} from './providers/web_workers/ui/auth';
+import {MessageBasedFirebaseAuth} from './auth/web_workers/ui/auth';
 
 export const WORKER_RENDER_FIREBASE_PROVIDERS: any[] = [
   COMMON_PROVIDERS,
