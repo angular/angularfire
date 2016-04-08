115e76a4f61b42d717d3ebb842684c66e8ef001a
David East <deast@google.com>
2016-04-08 07:16:17 -0700

    fix(tests): Reporter dots

--- karma.conf.js
+++ karma.conf.js
@@ -91,7 +91,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots'],
 
 
     // web server port
