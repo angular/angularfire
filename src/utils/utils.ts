aba1533a830a475fb76da7249db5762498d763df
David East <deast@google.com>
2016-04-11 16:44:21 -0700

    chore(utils): Add utils for runtime type checking

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,19 @@
 export function isPresent(obj: any): boolean {
   return obj !== undefined && obj !== null;
 }
+
+export function isString(value: any): boolean {
+  return typeof value === 'string';
+}
+
+export function isFirebaseRef(value: any): boolean {
+  return value instanceof Firebase;
+}
+
+export function isFirebaseDataSnapshot(value: any): boolean {
+  return typeof value.key === 'function';
+}
+
+export function isAFUnwrappedSnapshot(value: any): boolean {
+  return typeof value.$key === 'string';
+}
