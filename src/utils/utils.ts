47943c69c181aa0da04c1de737f756e5d69bb32b
David East <deast@google.com>
2016-04-12 06:04:02 -0700

    chore(utils): function for checking for url or firebase ref

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -17,3 +17,18 @@ export function isFirebaseDataSnapshot(value: any): boolean {
 export function isAFUnwrappedSnapshot(value: any): boolean {
   return typeof value.$key === 'string';
 }
+
+export interface CheckUrlRef {
+  isUrl: () => any;
+  isRef: () => any;
+}
+
+export function checkForUrlOrFirebaseRef(urlOrRef: string | Firebase, cases: CheckUrlRef): boolean {
+  if (isString(urlOrRef)) {
+    return cases.isUrl();
+  }
+  if (isFirebaseRef(urlOrRef)) {
+    return cases.isRef();
+  }
+  throw new Error('Provide a url or a Firebase database reference');  
+}
