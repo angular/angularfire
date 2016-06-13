5bde0578545ecb8434163900fa75f7a522f9cc3b
David East <deast@google.com>
2016-06-13 16:03:15 -0700

    fix(database): make database work with new Firebase SDK

--- src/utils/firebase_list_observable.ts
+++ src/utils/firebase_list_observable.ts
@@ -4,17 +4,22 @@ import {Subscriber} from 'rxjs/Subscriber';
 import {Subscription} from 'rxjs/Subscription';
 import * as utils from './utils';
 
-export type FirebaseOperation = string | Firebase | FirebaseDataSnapshot | AFUnwrappedDataSnapshot
-
 export interface FirebaseOperationCases {
-  stringCase: () => Promise<void>;
-  firebaseCase?: () => Promise<void>;
-  snapshotCase?: () => Promise<void>;
-  unwrappedSnapshotCase?: () => Promise<void>;
+  stringCase: () => firebase.Promise<void>;
+  firebaseCase?: () => firebase.Promise<void>;
+  snapshotCase?: () => firebase.Promise<void>;
+  unwrappedSnapshotCase?: () => firebase.Promise<void>;
 }
 
+export interface AFUnwrappedDataSnapshot {
+  $key: string;
+  $value?: string | number | boolean;
+}
+
+export type FirebaseOperation = string | firebase.database.Reference | firebase.database.DataSnapshot | AFUnwrappedDataSnapshot;
+
 export class FirebaseListObservable<T> extends Observable<T> {
-  constructor(public _ref: Firebase | FirebaseQuery, subscribe?: <R>(subscriber: Subscriber<R>) => Subscription | Function | void) {
+  constructor(public _ref: firebase.database.Reference | firebase.database.Query, subscribe?: <R>(subscriber: Subscriber<R>) => Subscription | Function | void) {
     super(subscribe);
   }
   lift<T, R>(operator: Operator<T, R>): Observable<R> {
@@ -23,41 +28,42 @@ export class FirebaseListObservable<T> extends Observable<T> {
     observable.operator = operator;
     observable._ref = this._ref;
     return observable;
-  } 
+  }
 
-  push(val:any):FirebaseWithPromise<void> {
+  push(val:any):firebase.database.ThenableReference {
     if(!this._ref) {
       throw new Error('No ref specified for this Observable!');
     }
-    return this._ref.ref().push(val);
+    this._ref.ref
+    return this._ref.ref.push(val);
   }
-  
-  update(item: FirebaseOperation, value: Object): Promise<void> {
+
+  update(item: FirebaseOperation, value: Object): firebase.Promise<void> {
     return this._checkOperationCases(item, {
-      stringCase: () => this._ref.ref().child(<string>item).update(value),
-      firebaseCase: () => (<Firebase>item).update(value),
-      snapshotCase: () => (<FirebaseDataSnapshot>item).ref().update(value),
-      unwrappedSnapshotCase: () => this._ref.ref().child((<AFUnwrappedDataSnapshot>item).$key).update(value)
+      stringCase: () => this._ref.ref.child(<string>item).update(value),
+      firebaseCase: () => (<firebase.database.Reference>item).update(value),
+      snapshotCase: () => (<firebase.database.DataSnapshot>item).ref.update(value),
+      unwrappedSnapshotCase: () => this._ref.ref.child((<AFUnwrappedDataSnapshot>item).$key).update(value)
     });
   }
 
-  remove(item:FirebaseOperation = null): Promise<void> {
+  remove(item:FirebaseOperation = null): firebase.Promise<void> {
     // TODO: remove override when typings are updated to include
     // remove() returning a promise.
-    
+
     // if no item parameter is provided, remove the whole list
     if (!item) {
-      return this._ref.ref().remove();
+      return this._ref.ref.remove();
     }
     return this._checkOperationCases(item, {
-      stringCase: () => this._ref.ref().child(<string>item).remove(),
-      firebaseCase: () => (<Firebase>item).remove(),
-      snapshotCase: () => (<FirebaseDataSnapshot>item).ref().remove(),
-      unwrappedSnapshotCase: () => this._ref.ref().child((<AFUnwrappedDataSnapshot>item).$key).remove()
+      stringCase: () => this._ref.ref.child(<string>item).remove(),
+      firebaseCase: () => (<firebase.database.Reference>item).remove(),
+      snapshotCase: () => (<firebase.database.DataSnapshot>item).ref.remove(),
+      unwrappedSnapshotCase: () => this._ref.ref.child((<AFUnwrappedDataSnapshot>item).$key).remove()
     });
   }
-  
-  _checkOperationCases(item: FirebaseOperation, cases: FirebaseOperationCases) : Promise<void> {
+
+  _checkOperationCases(item: FirebaseOperation, cases: FirebaseOperationCases) : firebase.Promise<void> {
     if (utils.isString(item)) {
       return cases.stringCase();
     } else if (utils.isFirebaseRef(item)) {
@@ -72,10 +78,5 @@ export class FirebaseListObservable<T> extends Observable<T> {
     }
     throw new Error(`FirebaseListObservable.remove requires a key, snapshot, reference, or unwrapped snapshot. Got: ${typeof item}`);
   }
-  
-}
 
-export interface AFUnwrappedDataSnapshot {
-  $key: string;
-  $value?: string | number | boolean;
 }
--- src/utils/firebase_object_observable.ts
+++ src/utils/firebase_object_observable.ts
@@ -4,7 +4,7 @@ import {Subscriber} from 'rxjs/Subscriber';
 import {Subscription} from 'rxjs/Subscription';
 
 export class FirebaseObjectObservable<T> extends Observable<T> {
-  constructor(subscribe?: <R>(subscriber: Subscriber<R>) => Subscription | Function | void, private _ref?:Firebase) {
+  constructor(subscribe?: <R>(subscriber: Subscriber<R>) => Subscription | Function | void, private _ref?:firebase.database.Reference) {
     super(subscribe);
   }
   lift<T, R>(operator: Operator<T, R>): Observable<R> {
@@ -14,19 +14,19 @@ export class FirebaseObjectObservable<T> extends Observable<T> {
     observable._ref = this._ref;
     return observable;
   }
-  set(value: any): Promise<void> {
+  set(value: any): firebase.Promise<void> {
     if(!this._ref) {
       throw new Error('No ref specified for this Observable!');
     }
     return this._ref.set(value);
   }
-  update(value: Object): Promise<void> {
+  update(value: Object): firebase.Promise<void> {
     if(!this._ref) {
       throw new Error('No ref specified for this Observable!');
     }
     return this._ref.update(value);
   }
-  remove(): Promise<void> {
+  remove(): firebase.Promise<void> {
     if(!this._ref) {
       throw new Error('No ref specified for this Observable!');
     }
