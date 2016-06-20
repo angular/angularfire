2f9b234e0a3706d0b8962da7f5896b72423868ba
Jeff Cross <middlefloor@gmail.com>
2016-06-20 15:10:29 -0700

    test(e2e): fix typescript config and build of examples

--- test/e2e/firebase_list/firebase_list_example.ts
+++ test/e2e/firebase_list/firebase_list_example.ts
@@ -10,7 +10,15 @@ import {
 
 enableProdMode();
 
-import { COMMON_CONFIG } from '../../../src/test-config';
+// TODO fix imports and tsconfig
+// import { COMMON_CONFIG } from '../../../src/test-config';
+
+const COMMON_CONFIG = {
+    apiKey: "AIzaSyBVSy3YpkVGiKXbbxeK0qBnu3-MNZ9UIjA",
+    authDomain: "angularfire2-test.firebaseapp.com",
+    databaseURL: "https://angularfire2-test.firebaseio.com",
+    storageBucket: "angularfire2-test.appspot.com",
+};
 
 @Component({
   template: `
--- test/e2e/firebase_object/firebase_object_example.ts
+++ test/e2e/firebase_object/firebase_object_example.ts
@@ -8,7 +8,16 @@ import {
   FirebaseApp
 } from 'angularfire2';
 
-import { COMMON_CONFIG } from '../../../src/test-config';
+// TODO fix imports and tsconfig
+// import { COMMON_CONFIG } from '../../../src/test-config';
+
+const COMMON_CONFIG = {
+    apiKey: "AIzaSyBVSy3YpkVGiKXbbxeK0qBnu3-MNZ9UIjA",
+    authDomain: "angularfire2-test.firebaseapp.com",
+    databaseURL: "https://angularfire2-test.firebaseio.com",
+    storageBucket: "angularfire2-test.appspot.com",
+};
+
 
 enableProdMode();
 
