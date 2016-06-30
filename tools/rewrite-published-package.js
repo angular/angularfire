f552d41fe0c92529a7cb82d0cdedfe3a71fdfca0
Jeff Cross <middlefloor@gmail.com>
2016-06-30 14:53:57 -0700

    chore(package): make firebase a dependency instead of peerDep in published package
    Fixes #311

--- tools/rewrite-published-package.js
+++ tools/rewrite-published-package.js
@@ -6,12 +6,24 @@
 var fs = require('fs');
 var srcPackage = require('../package.json');
 var [MAIN, JSNEXT_MAIN] = ['main', 'jsnext:main'].map(k => srcPackage[k].replace('/dist/', '/'));
+
+var peerDependencies = Object.assign({}, srcPackage.dependencies);
+// See note about including firebase as dependency
+delete peerDependencies.firebase;
 var outPackage = Object.assign({}, srcPackage, {
-  peerDependencies: srcPackage.dependencies,
+  peerDependencies,
   main: MAIN,
   typings: "angularfire2.d.ts",
   "jsnext:main": JSNEXT_MAIN,
-  dependencies: undefined
+  dependencies: {
+    /**
+     * Firebase SDK should be a dependency since it's not required that
+     * projects will install/use the SDK directly. And since Firebase uses
+     * semver, the version restriction is more permissive. This means if the user
+     * has installed Firebase already, npm is more likely to find a matching version.
+     **/
+     firebase: srcPackage.dependencies.firebase
+  }
 });
 
 fs.writeFileSync('./dist/package.json', JSON.stringify(outPackage, null, 2));
