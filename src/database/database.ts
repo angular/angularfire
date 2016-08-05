5720ebc48fddb2d1e75cd905fb534c4ed2e5c286
Nicholas Jamieson <nicholas@cartant.com>
2016-08-06 04:15:15 +1000

    Injected the Firebase App into AngularFireDatabase (#355)
    * Injected the Firebase App into AngularFireDatabase
    
    * Reverted the changes to the factories
    
    * Needed to call refFromURL - not ref

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
-import { FirebaseConfig } from '../tokens';
+import { FirebaseApp, FirebaseConfig } from '../tokens';
 import { FirebaseAppConfig } from '../angularfire2';
 import { FirebaseListFactory } from './index';
 import { FirebaseListFactoryOpts, FirebaseObjectFactoryOpts } from '../interfaces';
@@ -12,16 +12,17 @@ import {
 
 @Injectable()
 export class AngularFireDatabase {
-  constructor(@Inject(FirebaseConfig) private fbConfig:FirebaseAppConfig) {}
+  constructor(@Inject(FirebaseConfig) private fbConfig:FirebaseAppConfig,
+    @Inject(FirebaseApp) private fbApp:firebase.app.App) {}
   list (urlOrRef:string | firebase.database.Reference, opts?:FirebaseListFactoryOpts):FirebaseListObservable<any[]> {
     return utils.checkForUrlOrFirebaseRef(urlOrRef, {
-      isUrl: () => FirebaseListFactory(getAbsUrl(this.fbConfig, <string>urlOrRef), opts),
+      isUrl: () => FirebaseListFactory(this.fbApp.database().refFromURL(getAbsUrl(this.fbConfig, <string>urlOrRef)), opts),
       isRef: () => FirebaseListFactory(<firebase.database.Reference>urlOrRef)
     });
   }
   object(urlOrRef: string | firebase.database.Reference, opts?:FirebaseObjectFactoryOpts):FirebaseObjectObservable<any> {
     return utils.checkForUrlOrFirebaseRef(urlOrRef, {
-      isUrl: () => FirebaseObjectFactory(getAbsUrl(this.fbConfig, <string>urlOrRef), opts),
+      isUrl: () => FirebaseObjectFactory(this.fbApp.database().refFromURL(getAbsUrl(this.fbConfig, <string>urlOrRef)), opts),
       isRef: () => FirebaseObjectFactory(urlOrRef)
     });
   }
