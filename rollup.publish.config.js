125412a43303d1595fdc72bf45ef145a2493ad58
meDavid <david.git@jool.nl>
2016-09-21 13:57:28 +0200

    Fix in rollup config instead of package.json

--- rollup.publish.config.js
+++ rollup.publish.config.js
@@ -2,7 +2,7 @@ import globals from './rollup-globals';
 
 export default {
   entry: 'dist/index.js',
-  dest: 'dist/bundles/angularFire2.umd.js',
+  dest: 'dist/bundles/angularfire2.umd.js',
   format: 'umd',
   moduleName: 'angularFire2',
   globals
