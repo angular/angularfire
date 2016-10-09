5b818dfce6a500c9c7e287df9bb6c69c02d4c1e1
fabien0102 <fabien0102@planee.fr>
2016-10-09 21:43:50 +0200

    chore(firebaseObjectObservable) Add callback on set method

--- src/database/firebase_object_observable.ts
+++ src/database/firebase_object_observable.ts
@@ -25,7 +25,6 @@ export class FirebaseObjectObservable<T> extends Observable<T> {
     if(!this.$ref) {
       throw new Error('No ref specified for this Observable!');
     }
-    return this.$ref.set(value);
   }
 
   update(value: Object, onComplete?: (a: Object) => any): firebase.Promise<void> {
