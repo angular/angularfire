eb9368668bd80b74d1e7652e8ace0dc06881e05a
fabien0102 <fabien0102@planee.fr>
2016-11-15 16:45:33 +0100

    build(firebase): set firebase as peer dependency
    Avoid incompatibility with project firebase.
    
    Fixes #623

--- tools/rewrite-published-package.js
+++ tools/rewrite-published-package.js
@@ -9,20 +9,7 @@ var srcPackage = require('../package.json');
 delete srcPackage.scripts;
 
 var peerDependencies = Object.assign({}, srcPackage.dependencies);
-// See note about including firebase as dependency
-delete peerDependencies.firebase;
 
-var outPackage = Object.assign({}, srcPackage, {
-  peerDependencies,
-  dependencies: {
-    /**
-     * Firebase SDK should be a dependency since it's not required that
-     * projects will install/use the SDK directly. And since Firebase uses
-     * semver, the version restriction is more permissive. This means if the user
-     * has installed Firebase already, npm is more likely to find a matching version.
-     **/
-     firebase: srcPackage.dependencies.firebase
-  }
-});
+var outPackage = Object.assign({}, srcPackage, { peerDependencies });
 
 fs.writeFileSync('./dist/package.json', JSON.stringify(outPackage, null, 2));
