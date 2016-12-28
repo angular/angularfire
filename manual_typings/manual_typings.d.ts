bb99e8d9c7578db782b5c2a876334820e6df6407
David East <deast@google.com>
2016-12-28 05:48:12 -0800

    chore(build): Remove obsolete manual typings

--- manual_typings/manual_typings.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="mock-promises/mock-promises.d.ts" />
-/// <reference path="firebase3/firebase3.d.ts" />
---
+++ pack.sh
@@ -0,0 +1,2 @@
+# Creates a local build of AngularFire2
+npm run build_npm && npm pack dist
