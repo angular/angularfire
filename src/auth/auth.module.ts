398e4e263661b302ef8e2302d0de7c177f80e3ea
David East <david@firebase.com>
2017-03-12 22:57:10 -0700

    feat(rc): Implement rc0 API

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, NgZone } from '@angular/core';
 import * as firebase from 'firebase/app';
-import 'firebase/database';
+import 'firebase/auth';
 import { FirebaseApp } from '../app/index';
 import { AngularFireModule } from '../angularfire2';
 import { AngularFireAuth } from './index';
