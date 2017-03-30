0a8c8e39d84b11e0f6aa8d801cf63229f11612ea
David East <david@firebase.com>
2017-03-29 20:35:51 -0700

    chore(tests): Fix typings tests

--- test/typings-test/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This file just imports from a path that was previously known to having
- * typings errors because it relied on an ambient namespace for firebase,
- * which wouldn't necessarily have been loaded.
- */
-
-import {FirebaseSdkAuthBackend} from 'angularfire2/auth/firebase_sdk_auth_backend';
-
-const Backend = FirebaseSdkAuthBackend;
