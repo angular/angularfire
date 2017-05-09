b2aaa3ce18109fb58391800da69337971927038f
James Daniels <jamesdaniels@google.com>
2017-05-09 13:18:05 -0700

    Zap the $ on uploadTask

--- src/storage/firebase_upload_task_observable.ts
+++ src/storage/firebase_upload_task_observable.ts
@@ -7,32 +7,32 @@ import * as firebase from 'firebase/app';
 import 'firebase/storage';
 
 export class FirebaseUploadTaskObservable<T> extends Observable<T> {
-  constructor(subscribe?: <R>(subscriber: Subscriber<R>) => Subscription | Function | void, public $uploadTask?:firebase.storage.UploadTask) {
+  constructor(subscribe?: <R>(subscriber: Subscriber<R>) => Subscription | Function | void, public uploadTask?:firebase.storage.UploadTask) {
     super(subscribe);
   }
   lift<T, R>(operator: Operator<T, R>): Observable<R> {
     const observable = new FirebaseUploadTaskObservable<R>();
     observable.source = this;
     observable.operator = operator;
-    observable.$uploadTask = this.$uploadTask;
+    observable.uploadTask = this.uploadTask;
     return observable;
   }
   cancel(): boolean {
-    if(!this.$uploadTask) {
+    if(!this.uploadTask) {
       throw new Error('No uploadTask specified for this Observable!');
     }
-    return this.$uploadTask.cancel();
+    return this.uploadTask.cancel();
   }
   pause(): boolean {
-    if(!this.$uploadTask) {
+    if(!this.uploadTask) {
       throw new Error('No uploadTask specified for this Observable!');
     }
-    return this.$uploadTask.pause();
+    return this.uploadTask.pause();
   }
   resume(): boolean {
-    if(!this.$uploadTask) {
+    if(!this.uploadTask) {
       throw new Error('No uploadTask specified for this Observable!');
     }
-    return this.$uploadTask.resume();
+    return this.uploadTask.resume();
   }
 }
