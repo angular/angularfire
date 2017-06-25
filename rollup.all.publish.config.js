2792f5c9b2924a5cde2d6510d39101a220cbbc3e
doubleddm <diedrikdm@hotmail.com>
2017-06-25 12:51:36 +0200

    fix(FirebaseApp): Importing angularfire2/database and angularfire2/auth Removed the workaround. Adding some extra explanation to change the SystemJS.config to load firebase correctly
    fixes angular/angularfire2#992

--- rollup.all.publish.config.js
+++ rollup.all.publish.config.js
@@ -1,24 +1,9 @@
 import globals from './rollup-globals';
-import replace from 'rollup-plugin-re'
 
 export default {
   entry: 'dist/bundle.js',
   dest: 'dist/bundles/angularfire2.all.umd.js',
   format: 'umd',
   moduleName: 'angularfire2',
-  globals,
-   plugins: [
-    replace({
-      patterns: [
-        {
-          test: 'firebase.app(',
-          replace: 'firebase.firebase.app(',
-        },
-        {
-          test: 'firebase.initializeApp(',
-          replace: 'firebase.firebase.initializeApp(',
-        }
-      ]
-    })
-    ]
+  globals
 };
