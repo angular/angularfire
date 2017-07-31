8c4fa2e7a29777fe7a3306a44489303dacbfd3fa
David East <deast@google.com>
2017-07-31 14:28:45 -0600

    chore(build): clean up old files

--- test/e2e/shared.ts
+++
@@ -1,5 +0,0 @@
-export function waitForElement(selector: string) {
-  var EC = (protractor).ExpectedConditions;
-  // Waits for the element with id 'abc' to be present on the dom.
-  browser.wait(EC.presenceOf($(selector)), 20000);
-}
