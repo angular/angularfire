8ab38037df901030b378e4e7d881677e65169be5
James Daniels <jamesdaniels@google.com>
2017-10-03 21:30:02 -0700

    fix(auth): Clean up the authentication module

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -2,11 +2,12 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import { Injectable, NgZone } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { Observer } from 'rxjs/Observer';
 import { observeOn } from 'rxjs/operator/observeOn';
 import { FirebaseApp, ZoneScheduler } from 'angularfire2';
 
-export type Auth = firebase.auth.Auth;
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/fromPromise';
 
 @Injectable()
 export class AngularFireAuth {
@@ -14,54 +15,34 @@ export class AngularFireAuth {
   /**
    * Firebase Auth instance
    */
-  auth: firebase.auth.Auth;
+  public readonly auth: firebase.auth.Auth;
 
   /**
    * Observable of authentication state; as of 4.0 this is only triggered via sign-in/out
    */
-  authState: Observable<firebase.User>;
+  public readonly authState: Observable<firebase.User|null>;
 
   /**
    * Observable of the signed-in user's ID token; which includes sign-in, sign-out, and token refresh events
    */
-  idToken: Observable<firebase.User>;
+  public readonly idToken: Observable<string|null>;
 
   constructor(public app: FirebaseApp) {
-    this.authState = FirebaseAuthStateObservable(app);
-    this.idToken = FirebaseIdTokenObservable(app);
     this.auth = app.auth();
+
+    const authState$ = new Observable(subscriber => {
+      const unsubscribe = this.auth.onAuthStateChanged(subscriber);
+      return { unsubscribe };
+    });
+    this.authState = observeOn.call(authState$, new ZoneScheduler(Zone.current));
+
+    const idToken$ = new Observable<firebase.User|null>(subscriber => {
+      const unsubscribe = this.auth.onIdTokenChanged(subscriber);
+      return { unsubscribe };
+    }).switchMap(user => {
+      return user ? Observable.fromPromise(user.getIdToken()) : Observable.of(null)
+    });
+    this.idToken = observeOn.call(idToken$, new ZoneScheduler(Zone.current));
   }
 
-}
-
-/**
- * Create an Observable of Firebase authentication state. Each event is called
- * within the current zone.
- * @param app - Firebase App instance
- */
-export function FirebaseAuthStateObservable(app: FirebaseApp): Observable<firebase.User> {
-  const authState = Observable.create((observer: Observer<firebase.User>) => {
-    app.auth().onAuthStateChanged(
-      (user: firebase.User) => observer.next(user!),
-      (error: firebase.auth.Error) => observer.error(error),
-      () => { observer.complete(); return undefined; }
-    );
-  });
-  return observeOn.call(authState, new ZoneScheduler(Zone.current));
-}
-
-/**
- * Create an Observable of Firebase ID token. Each event is called
- * within the current zone.
- * @param app - Firebase App instance
- */
-export function FirebaseIdTokenObservable(app: FirebaseApp): Observable<firebase.User> {
-  const idToken = Observable.create((observer: Observer<firebase.User>) => {
-    app.auth().onIdTokenChanged(
-      (user: firebase.User) => observer.next(user!),
-      (error: firebase.auth.Error) => observer.error(error),
-      () => { observer.complete(); return undefined; }
-    )
-  });
-  return observeOn.call(idToken, new ZoneScheduler(Zone.current));
 }
