35b9bfd97d294bf734ec5ce3ee119415ba83ccd4
James Daniels <jamesdaniels@google.com>
2017-10-05 15:48:39 -0700

    fix(afs): no longer need the changes#share

--- src/firestore/collection/changes.ts
+++ src/firestore/collection/changes.ts
@@ -5,7 +5,6 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/scan';
-import 'rxjs/add/operator/share';
 
 import { DocumentChangeAction, Action } from '../interfaces';
 
@@ -29,8 +28,7 @@ export function sortedChanges(query: firebase.firestore.Query, events: firebase.
   return fromCollectionRef(query)
     .map(changes => changes.payload.docChanges)
     .scan((current, changes) => combineChanges(current, changes, events), [])
-    .map(changes => changes.map(c => ({ type: c.type, payload: c })))
-    .share();
+    .map(changes => changes.map(c => ({ type: c.type, payload: c })));
 }
 
 /**
