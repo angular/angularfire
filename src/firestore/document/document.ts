c22638f0efdbbcf0ae0df986c3a02892debdde33
Mark Goho <markgoho@gmail.com>
2017-10-11 12:54:02 -0400

    Update document.ts

--- src/firestore/document/document.ts
+++ src/firestore/document/document.ts
@@ -48,7 +48,7 @@ export class AngularFirestoreDocument<T> {
    * Create or overwrite a single document.
    * @param data
    */
-  set(data): Promise<void> {
+  set(data: Partial<T>): Promise<void> {
     return this.ref.set(data);
   }
 
@@ -56,7 +56,7 @@ export class AngularFirestoreDocument<T> {
    * Update some fields of a document without overwriting the entire document.
    * @param data 
    */
-  update(data): Promise<void> {
+  update(data: Partial<T>): Promise<void> {
     return this.ref.update(data);
   }
 
