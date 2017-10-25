c33c3c31a08b07a4354132b529a2f1b1ad9d8efd
David East <davideast@users.noreply.github.com>
2017-10-25 15:02:52 -0600

    chore(build): add manual build verifier for Angular 5.0 (#1294)
    * chore(deps): upgrade typescript
    
    * chore(build): add manual build verifier for Angular 5.0

---
+++ test/ng-build/ng5/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp({}),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ test/ng-build/pack.sh
@@ -0,0 +1,1 @@
+npm pack ../../dist/packages-dist
