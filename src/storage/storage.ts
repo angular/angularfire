f868cc4f95cb22db4230677bc17c9fc9d1934a35
David East <deast@google.com>
2017-11-29 06:32:24 -0700

    feat(storage): Implement AngularFireStorage

---
+++ src/storage/storage.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { storage } from 'firebase/app';
+import 'firebase/storage';
+import { FirebaseApp } from 'angularfire2';
+import { AngularFireStorageRef } from './ref';
+import { AngularFireUploadTask } from './task';
+
+@Injectable()
+export class AngularFireStorage {
+  storage: storage.Storage;
+  
+    constructor(public app: FirebaseApp) {
+      this.storage = app.storage();
+    }
+
+    ref(path: string) {
+      return new AngularFireStorageRef(this.storage.ref(path));
+    }
+
+    upload(pathOrRef: string, data: any, metadata: storage.UploadMetadata | undefined) {
+      const storageRef = this.storage.ref(pathOrRef);
+      const ref = new AngularFireStorageRef(storageRef);
+      return ref.put(data, metadata);
+    }
+
+}
