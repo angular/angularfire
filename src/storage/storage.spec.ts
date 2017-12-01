4de5f717eaf8c72c07bd3293248f5ddf7555641c
David East <deast@google.com>
2017-11-30 20:54:23 -0700

    feat(storage): Metadata tests

--- src/storage/storage.spec.ts
+++ src/storage/storage.spec.ts
@@ -6,9 +6,7 @@ import { FirebaseApp, FirebaseAppConfig, AngularFireModule } from 'angularfire2'
 import { AngularFireStorageModule, AngularFireStorage } from 'angularfire2/storage';
 import { COMMON_CONFIG } from './test-config';
 
-console.log(AngularFireStorageModule);
-
-fdescribe('AngularFireStorage', () => {
+describe('AngularFireStorage', () => {
   let app: firebase.app.App;
   let afStorage: AngularFireStorage;
 
@@ -62,10 +60,32 @@ fdescribe('AngularFireStorage', () => {
       const blob = new Blob([JSON.stringify(data)], { type : 'application/json' });
       const ref = afStorage.ref('af.json');
       const task = ref.put(blob);
+      // Wait for the upload
       const sub = forkJoin(task.snapshotChanges())
+        // get the url download
         .mergeMap(() => ref.getDownloadURL())
+        // assert the URL
         .do(url => expect(url).toBeDefined())
+        // Delete the file
         .mergeMap(url => ref.delete())
+        // finish the test
+        .subscribe(done, done.fail);
+    });
+
+    it('should upload, get metadata, and delete', (done) => {
+      const data = { angular: "fire" };
+      const blob = new Blob([JSON.stringify(data)], { type : 'application/json' });
+      const ref = afStorage.ref('af.json');
+      const task = ref.put(blob, { customMetadata: { blah: 'blah' } });
+      // Wait for the upload
+      const sub = forkJoin(task.snapshotChanges())
+        // get the metadata download
+        .mergeMap(() => ref.getMetadata())
+        // assert the URL
+        .do(meta => expect(meta.customMetadata).toEqual({ blah: 'blah' }))
+        // Delete the file
+        .mergeMap(meta => ref.delete())
+        // finish the test
         .subscribe(done, done.fail);
     });
 
