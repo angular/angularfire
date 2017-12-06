30d373ebc70d4ec710912274614a94aa5176e436
David East <deast@google.com>
2017-12-06 15:25:24 -0700

    feat(storage): Fix RxJS operators mapping

--- tools/build.js
+++ tools/build.js
@@ -22,7 +22,7 @@ const GLOBALS = {
   'rxjs/operator/combineLatest': 'Rx.Observable.prototype',
   'rxjs/operator/merge': 'Rx.Observable.prototype',
   'rxjs/operator/map': 'Rx.Observable.prototype',
-  'rxjs/operators': 'Rx.Observable.prototype',
+  'rxjs/operators': 'Rx.operators',
   'rxjs/observable/of': 'Rx.Observable',
   'rxjs/observable/forkJoin': 'Rx.Observable',
   'rxjs/operator/auditTime': 'Rx.Observable.prototype',
