9da7f5554b3b18f7975a9165544f19b255de3217
James Daniels <james@jamesdaniels.net>
2018-01-23 12:51:36 -0800

    Fixed a typo here that works on my machine FSR

--- src/firebase-node/index.js
+++ src/firebase-node/index.js
@@ -1,4 +1,4 @@
 // Patches for loading angularfire in a Universal Context
 
 global['WebSocket'] = require("ws");
-global['XMLHttpRequest'] = require("XMLHttpRequest").XMLHttpRequest;
+global['XMLHttpRequest'] = require("xmlhttprequest").XMLHttpRequest;
