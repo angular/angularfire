d183fc44280b9df45c1dcfcbdc79dc4a0bf66ecf
James Daniels <jamesdaniels@google.com>
2018-05-11 18:06:15 -0700

    feat(): Support FirebaseAppConfig, clean up injection tokens

--- src/firestore/collection/collection.spec.ts
+++ src/firestore/collection/collection.spec.ts
@@ -1,4 +1,4 @@
-import { FirebaseApp, FirebaseAppConfig, AngularFireModule } from 'angularfire2';
+import { FirebaseApp, AngularFireModule } from 'angularfire2';
 import { AngularFirestore } from '../firestore';
 import { AngularFirestoreModule } from '../firestore.module';
 import { AngularFirestoreDocument } from '../document/document';
