ec6937f75f3202727dedc69d1f827677e9d12d95
James Daniels <jamesdaniels@google.com>
2018-08-10 00:35:45 -0700

    Zone.js and DI on top of this work, also fixing a couple small things

--- src/messaging/messaging.ts
+++ src/messaging/messaging.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { FirebaseApp } from 'angularfire2';
+import { Injectable, Inject, Optional, NgZone, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { messaging } from 'firebase';
 import { requestPermission } from './observable/request-permission';
-import { from } from 'rxjs';
-import { mergeMap } from 'rxjs/operators';
+import { Observable, empty, from, of, throwError } from 'rxjs';
+import { mergeMap, catchError } from 'rxjs/operators';
+import { FirebaseOptions, FirebaseAppConfig } from 'angularfire2';
+import { FirebaseOptionsToken, FirebaseNameOrConfigToken, _firebaseAppFactory, FirebaseZoneScheduler } from 'angularfire2';
 
 @Injectable()
 export class AngularFireMessaging {
@@ -14,31 +15,61 @@ export class AngularFireMessaging {
   tokenChanges: Observable<string|null>;
   messages: Observable<{}>;
   requestToken: Observable<string|null>;
+  deleteToken: (string) => Observable<boolean>;
 
-  constructor(public app: FirebaseApp) {
-    this.messaging = app.messaging();
-
-    this.requestPermission = requestPermission(this.messaging);
-
-    this.getToken = from(this.messaging.getToken());
-
-    this.tokenChanges = new Observable(subscriber => {
-      this.messaging.getToken().then(t => subscriber.next(t));
-      this.messaging.onTokenRefresh(subscriber);
+  constructor(
+    @Inject(FirebaseOptionsToken) options:FirebaseOptions,
+    @Optional() @Inject(FirebaseNameOrConfigToken) nameOrConfig:string|FirebaseAppConfig|undefined,
+    @Inject(PLATFORM_ID) platformId: Object,
+    zone: NgZone
+  ) {
+    const scheduler = new FirebaseZoneScheduler(zone, platformId);
+    this.messaging = zone.runOutsideAngular(() => {
+      const app = _firebaseAppFactory(options, nameOrConfig);
+      return app.messaging();
     });
 
-    this.messages = new Observable(subscriber => {
-      this.messaging.onMessage(subscriber);
-    });
-    
-    this.requestToken = this.requestPermission.pipe(
-      mergeMap(() => this.tokenChanges)
+    if (isPlatformBrowser(platformId)) {
+
+      this.requestPermission = scheduler.runOutsideAngular(
+        requestPermission(this.messaging)
+      );
+
+      this.getToken = scheduler.runOutsideAngular(
+        from(this.messaging.getToken())
+      );
+
+      this.tokenChanges = scheduler.runOutsideAngular(
+        new Observable(subscriber => {
+          this.messaging.getToken().then(t => subscriber.next(t));
+          this.messaging.onTokenRefresh(subscriber.next);
+        })
+      );
+
+      this.messages = scheduler.runOutsideAngular(
+        new Observable(subscriber => {
+          this.messaging.onMessage(subscriber.next);
+        })
+      );
+
+      this.requestToken = this.requestPermission.pipe(
+        catchError(() => of(null)),
+        mergeMap(() => this.tokenChanges),
+      );
+
+    } else {
+
+      this.requestPermission = throwError('Not available on server platform.');
+      this.getToken = of(null);
+      this.tokenChanges = of(null);
+      this.messages = empty();
+      this.requestToken = of(null);
+
+    }
+
+    this.deleteToken = (token: string) => scheduler.runOutsideAngular(
+      from(this.messaging.deleteToken(token))
     );
-
-  }
-
-  deleteToken(token: string) {
-    return from(this.messaging.deleteToken(token)!);
   }
 
 }
