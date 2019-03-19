18a09897a6d298968c2cdd9d369b8257ee1768de
stuart <s2.d.young@gmail.com>
2019-03-19 13:16:42 -0500

    Misnamed method:  updateMetatdata should be updateMetadata. Keeping old method name for short-term, backward compatibility?

--- src/storage/ref.ts
+++ src/storage/ref.ts
@@ -8,6 +8,7 @@ export interface AngularFireStorageReference {
   getMetadata(): Observable<any>;
   delete(): Observable<any>;
   child(path: string): any;
+  updateMetatdata(meta: SettableMetadata): Observable<any>;
   updateMetadata(meta: SettableMetadata): Observable<any>;
   put(data: any, metadata?: UploadMetadata | undefined): AngularFireUploadTask;
   putString(data: string, format?: string | undefined, metadata?: UploadMetadata | undefined): AngularFireUploadTask;
@@ -32,6 +33,7 @@ export function createStorageRef(ref: Reference, scheduler: FirebaseZoneSchedule
     ),
     delete: () => from(ref.delete()),
     child: (path: string) => createStorageRef(ref.child(path), scheduler),
+    updateMetatdata: (meta: SettableMetadata) => from(ref.updateMetatdata(meta)),
     updateMetadata: (meta: SettableMetadata) => from(ref.updateMetadata(meta)),
     put: (data: any, metadata?: UploadMetadata) => {
       const task = ref.put(data, metadata);
