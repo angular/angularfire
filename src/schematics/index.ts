f410687daecb898ef2bc47fedd983010844d19d7
kirjs <kirill@kcherkashin.org>
2019-04-24 18:17:03 -0400

    Set up builder/schematic in package.json Make sure everything works 🔥

--- src/schematics/index.ts
+++ src/schematics/index.ts
@@ -1,17 +1,17 @@
 import { Tree } from '@angular-devkit/schematics';
 import { from } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { Project } from './interfaces';
 import { ngAdd } from './ng-add';
 import { listProjects, projectPrompt } from './utils';
 
 interface DeployOptions {
-    project: string;
+  project: string;
 }
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export const ngDeploy = ({project}: DeployOptions) => (host: Tree) => from(listProjects()).pipe(
-    map((projects: Project[]) => projectPrompt(projects)),
-    map(({firebaseProject}: any) => ngAdd(host, {firebaseProject, project}))
-)
+  switchMap((projects: Project[]) => projectPrompt(projects)),
+  map(({firebaseProject}: any) => ngAdd(host, {firebaseProject, project}))
+);
---
+++ src/schematics/ng-add.spec.ts
@@ -0,0 +1,7 @@
+import 'jasmine';
+
+describe('ng add', () => {
+    it('adds', () => {
+      expect(1).toBe(1);
+    });
+});
