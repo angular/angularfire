e7a683cacf7512c5ac13a7aefbab898ca0acebbb
James Daniels <jamesdaniels@google.com>
2019-05-18 23:56:24 -0700

    Bump to 5.2 and get the version from the package.json

--- tools/build.sh
+++ tools/build.sh
@@ -5,7 +5,7 @@ yarn
 if test $TAG_NAME; then
     export VERSION=$(echo $TAG_NAME | sed 's/^v\(.*\)$/\1/')
 else
-    export VERSION=$(npm view @angular/fire version)-canary.$SHORT_SHA
+    export VERSION=$(npm version | head -n 1 |  sed "s/^.*: '\([^']*\).*/\1/")-canary.$SHORT_SHA
 fi
 
 npm version $VERSION
