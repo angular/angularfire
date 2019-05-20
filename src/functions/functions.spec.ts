d12b4c5f602a88477ee57f8a00a524f6a768db01
James Daniels <jamesdaniels@google.com>
2019-05-20 13:47:52 -0700

    feat(functions): Allow configuration of Functions Emulator Origin (#2017)
    * Allow use of Cloud Functions emulator via FUNCTIONS_ORIGIN DI token
    * Rearranged DI docs and added emulator origin
    * Export FUNCTIONS_REGION as an alternative to FunctionsRegionToken

--- src/functions/functions.spec.ts
+++ src/functions/functions.spec.ts
@@ -1,7 +1,7 @@
 import { ReflectiveInjector, Provider } from '@angular/core';
 import { TestBed, inject } from '@angular/core/testing';
 import { FirebaseApp, FirebaseOptionsToken, AngularFireModule, FirebaseNameOrConfigToken } from '@angular/fire';
-import { AngularFireFunctions, AngularFireFunctionsModule, FunctionsRegionToken } from '@angular/fire/functions';
+import { AngularFireFunctions, AngularFireFunctionsModule, FUNCTIONS_REGION, FUNCTIONS_ORIGIN } from '@angular/fire/functions';
 import { COMMON_CONFIG } from './test-config';
 
 describe('AngularFireFunctions', () => {
@@ -51,7 +51,8 @@ describe('AngularFireFunctions with different app', () => {
       providers: [
         { provide: FirebaseNameOrConfigToken, useValue: FIREBASE_APP_NAME_TOO },
         { provide: FirebaseOptionsToken, useValue: COMMON_CONFIG },
-        { provide: FunctionsRegionToken, useValue: 'asia-northeast1' },
+        { provide: FUNCTIONS_ORIGIN, useValue: 'http://0.0.0.0:9999' },
+        { provide: FUNCTIONS_REGION, useValue: 'asia-northeast1' }
       ]
     });
     inject([FirebaseApp, AngularFireFunctions], (app_: FirebaseApp, _fns: AngularFireFunctions) => {
