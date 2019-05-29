ea5c076bee2e1f9f4d8ee4fe5fd21b0e92f4a6ba
James Daniels <jamesdaniels@google.com>
2019-05-29 13:44:55 +0300

    Using a provider for perf

--- src/performance/performance.module.ts
+++ src/performance/performance.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { AngularFirePerformance } from './performance';
+import { NgModule, Optional, ApplicationRef, NgZone } from '@angular/core';
+import { AngularFirePerformance, AUTOMATICALLY_TRACE_CORE_NG_METRICS, INSTRUMENTATION_ENABLED, DATA_COLLECTION_ENABLED } from './performance';
+import { FirebaseApp } from '@angular/fire';
 
 import 'firebase/performance';
 
+const AngularFirePerformanceProvider = {
+  provide: AngularFirePerformance,
+  deps: [
+      FirebaseApp,
+      [new Optional(), AUTOMATICALLY_TRACE_CORE_NG_METRICS],
+      [new Optional(), INSTRUMENTATION_ENABLED],
+      [new Optional(), DATA_COLLECTION_ENABLED],
+      ApplicationRef,
+      NgZone
+  ]
+};
+
 @NgModule({
-  providers: [ AngularFirePerformance ]
+  providers: [ AngularFirePerformanceProvider ]
 })
 export class AngularFirePerformanceModule {
   constructor(_: AngularFirePerformance) {
