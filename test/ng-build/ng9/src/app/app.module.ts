50ca0d7b12656fd1b479f49b9d9779f8158a332c
James Daniels <james@jamesdaniels.net>
2019-11-11 17:46:51 -0800

    Getting ready to test ng9

---
+++ test/ng-build/ng9/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { FirebaseApp, AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+import { AngularFireFunctionsModule } from '@angular/fire/functions';
+import { AngularFirePerformanceModule, AUTOMATICALLY_TRACE_CORE_NG_METRICS } from '@angular/fire/performance';
+import { AngularFireAuthGuardModule } from '@angular/fire/auth-guard';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAwRrxjjft7KMdhwfLKPkd8PCBR3JFaLfo",
+      authDomain: "angularfirestore.firebaseapp.com",
+      databaseURL: "https://angularfirestore.firebaseio.com",
+      projectId: "angularfirestore",
+      storageBucket: "angularfirestore.appspot.com",
+      messagingSenderId: "1039984584356"
+    }),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireMessagingModule,
+    AngularFireFunctionsModule,
+    AngularFirePerformanceModule,
+    AngularFireAuthGuardModule
+  ],
+  providers: [{
+    provide: AUTOMATICALLY_TRACE_CORE_NG_METRICS, useValue: false
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
