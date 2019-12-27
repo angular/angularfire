74144688c53671d74c3717689bddce4a4f76033b
unnhao <unnhao@gmail.com>
2019-12-27 18:03:07 +0800

    add sotrage list all function

--- src/storage/interfaces.ts
+++ src/storage/interfaces.ts
@@ -7,3 +7,4 @@ export type UploadMetadata = storage.UploadMetadata;
 export type SettableMetadata = storage.SettableMetadata;
 export type Reference = storage.Reference;
 export type StringFormat = storage.StringFormat;
+export type ListResult = storage.ListResult;
