74369e35b9ea64003bd0bbc744bf84ae98044a24
James Daniels <jamesdaniels@google.com>
2020-01-30 12:39:31 -0800

    Make the auth guard pure

--- src/auth-guard/auth-guard.module.ts
+++ src/auth-guard/auth-guard.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { AngularFireAuthGuard } from './auth-guard';
-import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
-  imports: [ AngularFireAuthModule ],
   providers: [ AngularFireAuthGuard ]
 })
 export class AngularFireAuthGuardModule { }
--- src/auth-guard/auth-guard.ts
+++ src/auth-guard/auth-guard.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, Optional, NgZone } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable, of, pipe, UnaryFunction } from 'rxjs';
-import { map, switchMap, take } from 'rxjs/operators'
+import { map, switchMap, take, observeOn, shareReplay } from 'rxjs/operators'
 import { User } from 'firebase/app';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { ɵAngularFireSchedulers, FirebaseOptions, FirebaseAppConfig, FIREBASE_OPTIONS, FIREBASE_APP_NAME, ɵfirebaseAppFactory } from '@angular/fire';
 
 export type AuthPipeGenerator = (next: ActivatedRouteSnapshot, state: RouterStateSnapshot) => AuthPipe;
 export type AuthPipe = UnaryFunction<Observable<User|null>, Observable<boolean|any[]>>;
@@ -13,14 +13,33 @@ export type AuthPipe = UnaryFunction<Observable<User|null>, Observable<boolean|a
 })
 export class AngularFireAuthGuard implements CanActivate {
 
-  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+  authState: Observable<User|null>;
+
+  constructor(
+    @Inject(FIREBASE_OPTIONS) options:FirebaseOptions,
+    @Optional() @Inject(FIREBASE_APP_NAME) nameOrConfig:string|FirebaseAppConfig|null|undefined,
+    zone: NgZone,
+    private router: Router
+  ) {
+    const auth = of(undefined).pipe(
+      observeOn(new ɵAngularFireSchedulers(zone).outsideAngular),
+      switchMap(() => zone.runOutsideAngular(() => import('firebase/auth'))),
+      map(() => ɵfirebaseAppFactory(options, zone, nameOrConfig)),
+      map(app => app.auth()),
+      shareReplay({ bufferSize: 1, refCount: false }),
+    );
+
+    this.authState = auth.pipe(
+      switchMap(auth => new Observable<User|null>(auth.onAuthStateChanged.bind(auth)))
+    );
+  }
 
   canActivate = (next: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-    const authPipeFactory: AuthPipeGenerator = next.data.authGuardPipe || (() => loggedIn);
-    return this.afAuth.user.pipe(
-        take(1),
-        authPipeFactory(next, state),
-        map(can => typeof can == "boolean" ? can : this.router.createUrlTree(<any[]>can))
+    const authPipeFactory = next.data.authGuardPipe as AuthPipeGenerator || (() => loggedIn);
+    return this.authState.pipe(
+      take(1),
+      authPipeFactory(next, state),
+      map(can => typeof can == "boolean" ? can : this.router.createUrlTree(<any[]>can))
     );
   }
 
