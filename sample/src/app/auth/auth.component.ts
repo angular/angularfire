2f26a6d0bc82891194166061a1a84a140d2e740d
James Daniels <james@jamesdaniels.net>
2020-03-09 22:10:15 -0700

    Adding a sample repo

---
+++ sample/src/app/auth/auth.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  template: `
+    <p>
+      auth works!
+    </p>
+  `,
+  styles: []
+})
+export class AuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ sample/src/app/database/database.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
+import { startWith, tap } from '../../../node_modules/rxjs/operators';
+
+@Component({
+  selector: 'app-database',
+  template: `
+    <p>
+      Database!
+      {{ testObjectValue$ | async | json }}
+    </p>
+  `,
+  styles: []
+})
+export class DatabaseComponent implements OnInit {
+
+  public readonly testObjectValue$: Observable<any>;
+
+  constructor(state: TransferState, database: AngularFireDatabase) {
+    const doc = database.object('test');
+    const key = makeStateKey(doc.query.toString());
+    const existing = state.get(key, undefined);
+    this.testObjectValue$ = doc.valueChanges().pipe(existing ? startWith(existing) : tap(it => state.set(key, it)));
+  }
+
+  ngOnInit(): void {
+  }
+
+}
