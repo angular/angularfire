f178fddbdcd63ca2fa00a95a53a22c1a8b4115fe
HaoLi <unnhao@gmail.com>
2020-03-31 03:40:14 +0800

    feat(storage): add listAll function (#2271)

--- src/storage/ref.ts
+++ src/storage/ref.ts
@@ -1,4 +1,4 @@
-import { SettableMetadata, UploadMetadata, Reference, StringFormat } from './interfaces';
+import { SettableMetadata, UploadMetadata, Reference, StringFormat, ListResult} from './interfaces';
 import { createUploadTask, AngularFireUploadTask } from './task';
 import { Observable, from } from 'rxjs';
 import { ɵAngularFireSchedulers } from '@angular/fire';
@@ -12,6 +12,7 @@ export interface AngularFireStorageReference {
   updateMetadata(meta: SettableMetadata): Observable<any>;
   put(data: any, metadata?: UploadMetadata | undefined): AngularFireUploadTask;
   putString(data: string, format?: string | undefined, metadata?: UploadMetadata | undefined): AngularFireUploadTask;
+  listAll(): Observable<ListResult>;
 }
 
 /**
@@ -42,6 +43,7 @@ export function createStorageRef(
     putString: (data: string, format?: StringFormat, metadata?: UploadMetadata) => {
       const task = ref.putString(data, format, metadata);
       return createUploadTask(task);
-    }
+    },
+    listAll: () => from(ref.listAll())
   };
 }
