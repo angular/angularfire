d8769ae1488f49030f1fd41681678dc6d5ca0483
James Daniels <jamesdaniels@google.com>
2020-12-03 12:51:05 -0500

    fix(storage): fromTask on a running task should work as expected

--- src/storage/observable/fromTask.ts
+++ src/storage/observable/fromTask.ts
@@ -1,5 +1,4 @@
 import { Observable } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
 import { UploadTask, UploadTaskSnapshot } from '../interfaces';
 
 // need to import, else the types become import('firebase/app').default.storage.UploadTask
@@ -11,14 +10,27 @@ export function fromTask(task: UploadTask) {
     const progress = (snap: UploadTaskSnapshot) => subscriber.next(snap);
     const error = e => subscriber.error(e);
     const complete = () => subscriber.complete();
-    task.on('state_changed', progress, (e) => {
-      progress(task.snapshot);
-      error(e);
-    }, () => {
-      progress(task.snapshot);
-      complete();
-    });
-  }).pipe(
-    shareReplay({ bufferSize: 1, refCount: false })
-  );
+    progress(task.snapshot);
+    switch (task.snapshot.state) {
+      case firebase.storage.TaskState.SUCCESS:
+      case firebase.storage.TaskState.CANCELED:
+        complete();
+        break;
+      case firebase.storage.TaskState.ERROR:
+        error(new Error('task was already in error state'));
+        break;
+      default:
+        // on's type if Function, rather than () => void, need to wrap
+        const unsub = task.on('state_changed', progress, (e) => {
+          progress(task.snapshot);
+          error(e);
+        }, () => {
+          progress(task.snapshot);
+          complete();
+        });
+        return function unsubscribe() {
+          unsub();
+        };
+    }
+  });
 }
