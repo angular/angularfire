984006d83458df9f917af0b5317d2dbe8dd203d1
James Daniels <jamesdaniels@google.com>
2020-12-03 12:44:17 -0500

    fix(storage): Fix for typing issue with Firebase v7 (#2703)
    * Faster better stronger tests
    * Found the typing issue with Firebase v7, thanks matrix tests!

--- src/storage/observable/fromTask.ts
+++ src/storage/observable/fromTask.ts
@@ -2,6 +2,10 @@ import { Observable } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 import { UploadTask, UploadTaskSnapshot } from '../interfaces';
 
+// need to import, else the types become import('firebase/app').default.storage.UploadTask
+// and it no longer works w/Firebase v7
+import firebase from 'firebase/app';
+
 export function fromTask(task: UploadTask) {
   return new Observable<UploadTaskSnapshot>(subscriber => {
     const progress = (snap: UploadTaskSnapshot) => subscriber.next(snap);
