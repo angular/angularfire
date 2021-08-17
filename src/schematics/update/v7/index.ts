ec312e34fc50d2a9ef1727aca688e74581cdb08c
James Daniels <jamesdaniels@google.com>
2021-08-17 13:48:27 -0400

    Rework the import alias regex

--- src/schematics/update/v7/index.ts
+++ src/schematics/update/v7/index.ts
@@ -2,9 +2,14 @@ import { Rule, SchematicContext, SchematicsException, Tree } from '@angular-devk
 import { overwriteIfExists, safeReadJSON, stringifyFormatted } from '../../ng-add-common';
 import { default as defaultDependencies, firebaseFunctions } from '../../versions.json';
 
-const FIREBASE_IMPORT_REGEX = /(?<key>import|export)\s+(?:(?<alias>[\w,{}\s\*]+)\s+from)?\s*(?:(["'])?firebase\/(?<ref>[@\w\s\\\/.-]+)\3?)\s*;/g;
-const AT_FIREBASE_IMPORT_REGEX = /(?<key>import|export)\s+(?:(?<alias>[\w,{}\s\*]+)\s+from)?\s*(?:(["'])?@firebase\/(?<ref>[@\w\s\\\/.-]+)\3?)\s*;/g;
-const ANGULAR_FIRE_IMPORT_REGEX = /(?<key>import|export)\s+(?:(?<alias>[\w,{}\s\*]+)\s+from)?\s*(?:(["'])?@angular\/fire\/(?<ref>[@\w\s\\\/.-]+)\3?)\s*;/g;
+const IMPORT_REGEX = /(?<key>import|export)\s+(?:(?<alias>[\w,{}\s\*]+)\s+from)?\s*(?:(?<quote>["'])?(?<ref>[@\w\s\\\/.-]+)\3?)\s*(?<term>[;\n])/g;
+interface ImportRegexMatch {
+    key: string;
+    alias: string;
+    ref: string;
+    quote: string;
+    term: string;
+}
 
 export const ngUpdate = (): Rule => (
     host: Tree,
@@ -49,20 +54,20 @@ export const ngUpdate = (): Rule => (
         if (!content) {
             return;
         }
-        let didChangeContent = false;
-        if (content.match(FIREBASE_IMPORT_REGEX)) {
-            content.replace(FIREBASE_IMPORT_REGEX, '$1 $2 from $3firebase/compat/$4$3;');
-            didChangeContent = true;
-        }
-        if (content.match(AT_FIREBASE_IMPORT_REGEX)) {
-            content.replace(AT_FIREBASE_IMPORT_REGEX, '$1 $2 from $3@firebase/compat/$4$3;');
-            didChangeContent = true;
-        }
-        if (content.match(ANGULAR_FIRE_IMPORT_REGEX)) {
-            content.replace(ANGULAR_FIRE_IMPORT_REGEX, '$1 $2 from $3@angular/fire/compat/$4$3;');
-            didChangeContent = true;
-        }
-        if (didChangeContent) {
+        const newContent = content.replace(IMPORT_REGEX, (substring, ...args) => {
+            const { alias, key, ref, quote, term }: ImportRegexMatch = args.pop();
+            if (ref.startsWith('@angular/fire') && !ref.startsWith('@angular/fire/compat')) {
+                return `${key} ${alias} from ${quote}${ref.replace('@angular/fire', '@angular/fire/compat')}${quote}${term}`;
+            }
+            if (ref.startsWith('firebase') && !ref.startsWith('firebase/compat')) {
+                return `${key} ${alias} from ${quote}${ref.replace('firebase', 'firebase/compat')}${quote}${term}`;
+            }
+            if (ref.startsWith('@firebase')) {
+                return `${key} ${alias} from ${quote}${ref.replace('@firebase', 'firebase')}${quote}${term}`;
+            }
+            return substring;
+        });
+        if (content !== newContent) {
             host.overwrite(filePath, content);
         }
     });
