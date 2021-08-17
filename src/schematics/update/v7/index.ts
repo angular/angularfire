2a3bd2aca9e0f32afaa715e2363446e6ad2895a9
James Daniels <jamesdaniels@google.com>
2021-08-17 15:07:36 -0400

    Write the new content

--- src/schematics/update/v7/index.ts
+++ src/schematics/update/v7/index.ts
@@ -68,7 +68,7 @@ export const ngUpdate = (): Rule => (
             return substring;
         });
         if (content !== newContent) {
-            host.overwrite(filePath, content);
+            overwriteIfExists(host, filePath, newContent);
         }
     });
 
