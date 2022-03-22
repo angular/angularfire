c4d9ecfb3d45ef91078973ef2ab63657db8efa48
James Daniels <jamesdaniels@google.com>
2022-03-22 11:40:08 -0400

    Compile jasmine entry

---
+++ tools/jasmine.ts
@@ -0,0 +1,29 @@
+// @ts-ignore
+import Jasmine from 'jasmine';
+
+import('reflect-metadata');
+import('zone.js/dist/zone-node');
+import('zone.js/dist/zone-testing');
+
+import { getTestBed } from '@angular/core/testing';
+import { platformServerTesting, ServerTestingModule } from '@angular/platform-server/testing';
+
+// @ts-ignore
+global.globalThis = require('globalthis/polyfill')();
+
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(
+    ServerTestingModule,
+    platformServerTesting()
+);
+
+const tests = new Jasmine({});
+tests.loadConfig({
+    spec_dir: '.',
+    spec_files: [
+        'dist/out-tsc/jasmine/**/*.jasmine.js',
+        'dist/out-tsc/jasmine/**/*.spec.js',
+    ]
+});
+
+tests.execute();
