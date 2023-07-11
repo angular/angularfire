34e89a4625646ff446f2a49c8d8bc489fc917655
robertIsaac <dev.robert.isaac@gmail.com>
2023-07-11 13:07:21 +0300

    fix: Bundle includes unused firebase modules (#3366)
    * fix: Bundle includes unused firebase modules
    
    `@angular/fire` core package always import these packages
    - firebase/remote-config
    - firebase/messaging
    - firebase/analytics
    which increase the total bundle size for any application using `@angular/fire`
    
    Fix #3075
    
    * Update src/remote-config/is-remote-config-supported-factory.ts
    
    * Update src/messaging/is-messaging-supported-factory.ts
    
    * Update src/analytics/is-analytics-supported-factory.ts
    
    ---------
    
    Co-authored-by: David East <davideast@users.noreply.github.com>

---
+++ src/remote-config/is-remote-config-supported-factory.ts
@@ -0,0 +1,20 @@
+import { isSupported } from 'firebase/remote-config';
+import { ɵisSupportedError } from '@angular/fire';
+
+const isRemoteConfigSupportedValueSymbol = '__angularfire_symbol__remoteConfigIsSupportedValue';
+const isRemoteConfigSupportedPromiseSymbol = '__angularfire_symbol__remoteConfigIsSupported';
+
+globalThis[isRemoteConfigSupportedPromiseSymbol] ||= isSupported().then(it =>
+  globalThis[isRemoteConfigSupportedValueSymbol] = it
+).catch(() =>
+  globalThis[isRemoteConfigSupportedValueSymbol] = false
+);
+
+export const isRemoteConfigSupportedFactory = {
+  async: () => globalThis[isRemoteConfigSupportedPromiseSymbol],
+  sync: () => {
+    const ret = globalThis[isRemoteConfigSupportedValueSymbol];
+    if (ret === undefined) { throw new Error(ɵisSupportedError('RemoteConfig')); }
+    return ret;
+  }
+};
--- src/remote-config/remote-config.module.ts
+++ src/remote-config/remote-config.module.ts
@@ -1,9 +1,10 @@
 import { NgModule, Optional, NgZone, InjectionToken, ModuleWithProviders, Injector, APP_INITIALIZER } from '@angular/core';
 import { RemoteConfig as FirebaseRemoteConfig } from 'firebase/remote-config';
-import { ɵgetDefaultInstanceOf, ɵAngularFireSchedulers, VERSION, ɵisRemoteConfigSupportedFactory } from '@angular/fire';
+import { ɵgetDefaultInstanceOf, ɵAngularFireSchedulers, VERSION } from '@angular/fire';
 import { RemoteConfig, RemoteConfigInstances, REMOTE_CONFIG_PROVIDER_NAME } from './remote-config';
 import { FirebaseApps, FirebaseApp } from '@angular/fire/app';
 import { registerVersion } from 'firebase/app';
+import { isRemoteConfigSupportedFactory } from './is-remote-config-supported-factory';
 
 export const PROVIDED_REMOTE_CONFIG_INSTANCES = new InjectionToken<RemoteConfig[]>('angularfire2.remote-config-instances');
 
@@ -11,14 +12,14 @@ export function defaultRemoteConfigInstanceFactory(
   provided: FirebaseRemoteConfig[]|undefined,
   defaultApp: FirebaseApp,
 ) {
-  if (!ɵisRemoteConfigSupportedFactory.sync()) { return null; }
+  if (!isRemoteConfigSupportedFactory.sync()) { return null; }
   const defaultRemoteConfig = ɵgetDefaultInstanceOf<FirebaseRemoteConfig>(REMOTE_CONFIG_PROVIDER_NAME, provided, defaultApp);
   return defaultRemoteConfig && new RemoteConfig(defaultRemoteConfig);
 }
 
 export function remoteConfigInstanceFactory(fn: (injector: Injector) => FirebaseRemoteConfig) {
   return (zone: NgZone, injector: Injector) => {
-    if (!ɵisRemoteConfigSupportedFactory.sync()) { return null; }
+    if (!isRemoteConfigSupportedFactory.sync()) { return null; }
     const remoteConfig = zone.runOutsideAngular(() => fn(injector));
     return new RemoteConfig(remoteConfig);
   };
@@ -46,7 +47,7 @@ const DEFAULT_REMOTE_CONFIG_INSTANCE_PROVIDER = {
     REMOTE_CONFIG_INSTANCES_PROVIDER,
     {
       provide: APP_INITIALIZER,
-      useValue: ɵisRemoteConfigSupportedFactory.async,
+      useValue: isRemoteConfigSupportedFactory.async,
       multi: true,
     },
   ]
