5f5fdd9dc91e3e8dccd8c97df8901364db00dc2c
Miles Malerba <mmalerba@google.com>
2024-08-13 02:55:25 +0000

    handle app-check error that crashes SSR

--- samples/advanced/src/app/app-check/app-check.component.ts
+++ samples/advanced/src/app/app-check/app-check.component.ts
@@ -1,36 +1,39 @@
+import { AsyncPipe, NgIf, SlicePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { keepUnstableUntilFirst } from '@angular/fire';
 import { AppCheck, getToken } from '@angular/fire/app-check';
 import { traceUntilFirst } from '@angular/fire/performance';
-import { Observable, from } from 'rxjs';
-import { share } from 'rxjs/operators';
-import { NgIf, AsyncPipe, SlicePipe } from '@angular/common';
+import { Observable, from, of } from 'rxjs';
+import { catchError, share } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-app-check',
-    template: `
+  selector: 'app-app-check',
+  template: `
     <p>
       App Check!
-      <code>{{ (change$ | async)?.token | slice:0:12 }}<ng-container *ngIf="(change$ | async) !== null">&hellip;</ng-container></code>
+      <code
+        >{{ (change$ | async)?.token | slice : 0 : 12
+        }}<ng-container *ngIf="(change$ | async) !== null"
+          >&hellip;</ng-container
+        ></code
+      >
     </p>
   `,
-    styles: [],
-    standalone: true,
-    imports: [NgIf, AsyncPipe, SlicePipe]
+  styles: [],
+  standalone: true,
+  imports: [NgIf, AsyncPipe, SlicePipe],
 })
 export class AppCheckComponent implements OnInit {
-
   readonly change$: Observable<any>;
 
   constructor(appCheck: AppCheck) {
     this.change$ = from(getToken(appCheck)).pipe(
-      <any>keepUnstableUntilFirst,
-      <any>traceUntilFirst('app-check'),
-      share(),
+      keepUnstableUntilFirst,
+      traceUntilFirst('app-check'),
+      catchError(() => of(null)),
+      share()
     );
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
